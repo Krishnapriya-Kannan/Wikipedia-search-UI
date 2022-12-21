bc99c948d8248f021bc70d61f52c9b8c68e84091
krishnapriya kannan <kskpriya01@gmail.com>
2022-12-21 22:10:43 +0530

    wikipedia-search-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [result, setResult] = useState({});
+
+  async function fetchData(val) {
+    const searchtext = val.trim().toLowerCase();
+    console.log(searchtext);
+    if (searchtext.length > 0) {
+      const apistring = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchtext}`;
+        const response = await fetch(apistring);
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        return await response.json();
+    }
+}
+async function handleSearch(e) {
+  const { value } = e.target;
+  setResult({ result: await fetchData(value) })
+  console.log(result)
+}
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wiki Search</h1>
+    <input type="text" placeholder="Search" className="search" onKeyUp={handleSearch} />
+   
+    <body>
+    {result.result && result.result.query.search.map((data,i) =>{
+    
+    let url=  `https://en.wikipedia.org/wiki/${data.title}`.trim();
+      console.log(url);
+      return(<div>
+        <center>
+        <table style={{width:'90%'}}>
+      <tr><a href={url} style={{textDecoration:'none',padding:'20px',color:'deepskyblue'}}>{data.title}</a></tr>  
+        </table>
+        </center>
+        </div>
+        
+        )})}
+ </body>
     </div>
   );
 }
